e2e11efa25789f67cc1d85f57a24fafafb149f0f
AntonShcherba <scherba891@gmail.com>
2025-05-30 02:40:18 +0300

    add static Config for IdentityServer setup

--- UserService/UserService.IdentityServer/Config.cs
+++ UserService/UserService.IdentityServer/Config.cs
@@ -4,9 +4,42 @@ namespace UserService.IdentityServer;
 
 public static class Config
 {
-    public static IEnumerable<IdentityResource> IdentityResources => [];
+    public static IEnumerable<IdentityResource> IdentityResources =>
+    [
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile()
+    ];
 
-    public static IEnumerable<ApiScope> ApiScopes => [];
+    public static IEnumerable<ApiScope> ApiScopes =>
+    [
+        new("api1", "My API")
+    ];
 
-    public static IEnumerable<Client> Clients => [];
+    
+    public static IEnumerable<Client> Clients =>
+    [
+        new()
+        {
+            ClientId = "bff_client",
+            ClientName = "BFF Client",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.Code,
+            RequirePkce = true,
+
+            RedirectUris = { "https://localhost:5000/signin-oidc" },
+            PostLogoutRedirectUris = { "http://localhost:4200" },
+
+            AllowedScopes =
+            {
+                "openid",
+                "profile",
+                "api1",
+                "offline_access"
+            },
+
+            AllowOfflineAccess = true,
+            RequireClientSecret = true
+        }
+    ];
 }
