a1f77c091f893afc20ff81a4dc6ed2a1ee8d8ebe
AntonShcherba <scherba891@gmail.com>
2025-05-30 02:49:39 +0300

    implement create account

---
+++ UserService/UserService.IdentityServer/Pages/Account/Create/InputModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UserService.IdentityServer.Pages.Account.Create;
+
+public class InputModel
+{
+    [Required] public string Username { get; set; } = string.Empty;
+
+    [Required] public string Password { get; set; } = string.Empty;
+
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+
+    public string? ReturnUrl { get; set; }
+
+    public string? Button { get; set; }
+}
