468364f0cda13a628b450656cf14a66180da4a9c
AntonShcherba <scherba891@gmail.com>
2025-05-30 02:37:36 +0300

    setup IdentityServer and ASP.NET Identity integration

--- UserService/UserService.IdentityServer/HostingExtensions.cs
+++ UserService/UserService.IdentityServer/HostingExtensions.cs
@@ -1,14 +1,51 @@
+using UserService.Business.Entities;
+using UserService.Business.Extensions;
+using UserService.DataAccess.Extensions;
+
 namespace UserService.IdentityServer;
 
 internal static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
+        builder.Services.AddBusinessServices()
+            .AddDataAccessServices(builder.Configuration);
+        
+        builder.Services.AddRazorPages();
+
+        builder.Services
+            .AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients)
+            .AddAspNetIdentity<ApplicationUser>()
+            .AddLicenseSummary();
+
         return builder.Build();
     }
 
     public static WebApplication ConfigurePipeline(this WebApplication app)
     {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+        
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
+        app.MapRazorPages()
+            .RequireAuthorization();
+
         return app;
     }
 }
