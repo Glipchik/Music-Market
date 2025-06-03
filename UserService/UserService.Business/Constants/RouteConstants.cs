80a4f94177a7a388f46ae620cdea96b21cd89a77
AntonShcherba <scherba891@gmail.com>
2025-06-03 14:34:01 +0300

    add constants

---
+++ UserService/UserService.Business/Constants/RouteConstants.cs
@@ -0,0 +1,15 @@
+namespace UserService.Business.Constants;
+
+public class RouteConstants
+{
+    public const string RedirectIndex = "/Redirect/Index";
+    public const string AccountLogout = "/Account/Logout/Index";
+    public const string AccountLoggedOut = "/Account/Logout/LoggedOut";
+    public const string AccountLogin = "/Account/Login/Index";
+    public const string AccountCreate = "/Account/Create/Index";
+    public const string ServerSideSessionsIndex = "/ServerSideSessions/Index";
+    public const string GrantsIndex = "/Grants/Index";
+    public const string DeviceIndex = "/Device/Index";
+    public const string ConsentIndex = "/Consent/Index";
+    public const string CibaConsent = "/Login";
+}
