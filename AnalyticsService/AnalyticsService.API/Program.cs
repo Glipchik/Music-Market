392b06e3c43ecb77ae28e0f438d42244cfabe38a
AntonShcherba <scherba891@gmail.com>
2025-06-05 14:05:46 +0300

    add env uploading to program.cs

--- AnalyticsService/AnalyticsService.API/Program.cs
+++ AnalyticsService/AnalyticsService.API/Program.cs
@@ -1,8 +1,12 @@
 using AnalyticsService.API.Middlewares;
 using AnalyticsService.Business.Extensions;
 using AnalyticsService.DataAccess.Extensions;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddBusinessServices(builder.Configuration);
 builder.Services.AddDataAccessServices(builder.Configuration);
