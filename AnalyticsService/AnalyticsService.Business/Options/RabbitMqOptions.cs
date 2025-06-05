f9615583c0897b1c65bedd86ca3fb67c6805daf0
AntonShcherba <scherba891@gmail.com>
2025-06-05 12:32:30 +0300

    add RabbitMqOptions poco class

---
+++ AnalyticsService/AnalyticsService.Business/Options/RabbitMqOptions.cs
@@ -0,0 +1,8 @@
+namespace AnalyticsService.Business.Options;
+
+public class RabbitMqOptions
+{
+    public string User { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Host { get; set; } = null!;
+}
