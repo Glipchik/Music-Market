5eea612ae867a39ae7cfb2374868466b71af3560
AntonShcherba <scherba891@gmail.com>
2025-07-07 17:37:53 +0300

    add InstrumentId

--- InstrumentService/InstrumentService.IntegrationTests/Constants/InstrumentTestConstants.cs
+++ InstrumentService/InstrumentService.IntegrationTests/Constants/InstrumentTestConstants.cs
@@ -3,4 +3,5 @@ namespace InstrumentService.IntegrationTests.Constants;
 public static class InstrumentTestConstants
 {
     public const string NonExistentInstrumentId = "507f1f77bcf86cd799439011";
+    public const string InstrumentId = "111111111111111111111111";
 }
