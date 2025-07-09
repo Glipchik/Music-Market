10bddceca73f429b5e0d23d84a335ea1d17bbefa
AntonShcherba <scherba891@gmail.com>
2025-07-09 19:35:12 +0300

    add migrations

---
+++ AnalyticsService/AnalyticsService.DataAccess/Data/Migrations/20250613115534_Change_InstrumentId_From_Guid_To_String.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnalyticsService.DataAccess.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_InstrumentId_From_Guid_To_String : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InstrumentId",
+                table: "InstrumentStats",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InstrumentId",
+                table: "InstrumentDailyStats",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "InstrumentId",
+                table: "InstrumentStats",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "InstrumentId",
+                table: "InstrumentDailyStats",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+        }
+    }
+}
---
+++ AnalyticsService/AnalyticsService.DataAccess/Data/Migrations/20250704181848_change_defult_schema.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnalyticsService.DataAccess.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_defult_schema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "analytics");
+
+            migrationBuilder.RenameTable(
+                name: "UserStats",
+                newName: "UserStats",
+                newSchema: "analytics");
+
+            migrationBuilder.RenameTable(
+                name: "InstrumentStats",
+                newName: "InstrumentStats",
+                newSchema: "analytics");
+
+            migrationBuilder.RenameTable(
+                name: "InstrumentDailyStats",
+                newName: "InstrumentDailyStats",
+                newSchema: "analytics");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "UserStats",
+                schema: "analytics",
+                newName: "UserStats");
+
+            migrationBuilder.RenameTable(
+                name: "InstrumentStats",
+                schema: "analytics",
+                newName: "InstrumentStats");
+
+            migrationBuilder.RenameTable(
+                name: "InstrumentDailyStats",
+                schema: "analytics",
+                newName: "InstrumentDailyStats");
+        }
+    }
+}
