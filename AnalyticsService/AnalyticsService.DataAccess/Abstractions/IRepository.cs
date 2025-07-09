42d8496db8a8f38b31a44c97a0a3832d8cbf394a
AntonShcherba <scherba891@gmail.com>
2025-07-09 19:42:38 +0300

    set generic id type in generic repository and implement AddRangeAsync method

--- AnalyticsService/AnalyticsService.DataAccess/Abstractions/IRepository.cs
+++ AnalyticsService/AnalyticsService.DataAccess/Abstractions/IRepository.cs
@@ -1,11 +1,12 @@
 namespace AnalyticsService.DataAccess.Abstractions;
 
-public interface IRepository<T> where T : class
+public interface IRepository<T, TId> where T : class
 {
-    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<T?> GetByIdAsync(TId id, CancellationToken cancellationToken);
     Task<List<T>> GetAllAsync(CancellationToken cancellationToken);
 
     Task AddAsync(T entity, CancellationToken cancellationToken);
+    Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken);
     void Update(T entity, CancellationToken cancellationToken);
     void Remove(T entity, CancellationToken cancellationToken);
 }
