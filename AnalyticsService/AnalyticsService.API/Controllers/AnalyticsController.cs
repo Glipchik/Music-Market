cc2d553dfaf44b36c087182a80bf172341064338
AntonShcherba <scherba891@gmail.com>
2025-07-10 12:12:39 +0300

    setup authorization defence to AnalyticsController endpoins and add GetTopViewedInstruments endpoint and change id typed from guid to string

--- AnalyticsService/AnalyticsService.API/Controllers/AnalyticsController.cs
+++ AnalyticsService/AnalyticsService.API/Controllers/AnalyticsController.cs
@@ -1,5 +1,6 @@
 using AnalyticsService.Business.Abstractions;
 using AnalyticsService.Business.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AnalyticsService.API.Controllers;
@@ -8,44 +9,56 @@ namespace AnalyticsService.API.Controllers;
 [Route("analytics")]
 public class AnalyticsController(IAnalyticsService analyticsService) : ControllerBase
 {
-    [HttpGet("instruments/{instrumentId:guid}/stats")]
-    public async Task<InstrumentStatResult> GetInstrumentStat(Guid instrumentId, CancellationToken cancellationToken)
+    [HttpGet("instruments/{instrumentId}/stats")]
+    [Authorize(Policy = "ReadAccess")]
+    public async Task<InstrumentStatResult> GetInstrumentStat(string instrumentId, CancellationToken cancellationToken)
     {
         var response = await analyticsService
             .GetInstrumentStatAsync(instrumentId, cancellationToken);
 
         return response;
     }
-    
-    [HttpGet("instruments/{instrumentId:guid}/daily-stats")]
-    public async Task<InstrumentDailyStatResult> GetInstrumentDailyStat(Guid instrumentId, [FromQuery] DateOnly date,
+
+    [HttpGet("instruments/{instrumentId}/daily-stats")]
+    [Authorize(Policy = "ReadAccess")]
+    public async Task<InstrumentDailyStatResult> GetInstrumentDailyStat(string instrumentId, [FromQuery] DateOnly date,
         CancellationToken cancellationToken)
     {
-        var dateOnly = date;
         var response = await analyticsService
             .GetInstrumentDailyStatByDateAsync(instrumentId, date, cancellationToken);
-        
+
         return response;
     }
 
     [HttpGet("instruments/{instrumentId}/stats-range")]
-    public async Task<List<InstrumentDailyStatResult>> GetInstrumentDailyStatsForRange(
-        Guid instrumentId,
+    [Authorize(Policy = "ReadAccess")]
+    public async Task<List<InstrumentDailyStatResult>> GetInstrumentDailyStatsForDateRange(
+        string instrumentId,
         [FromQuery] DateOnly startDate,
         [FromQuery] DateOnly endDate,
         CancellationToken cancellationToken)
     {
         var response = await analyticsService
             .GetInstrumentDailyStatsByDateRangeAsync(instrumentId, startDate, endDate, cancellationToken);
-        
+
         return response;
     }
 
     [HttpGet("users/{userId}/stats")]
-    public async Task<UserStatResult> GetUserStats(Guid userId, CancellationToken cancellationToken)
+    public async Task<UserStatResult> GetUserStats(string userId, CancellationToken cancellationToken)
     {
         var response = await analyticsService.GetUserStatAsync(userId, cancellationToken);
-        
+
+        return response;
+    }
+
+    [HttpGet("instruments/top")]
+    [Authorize(Policy = "ReadAccess")]
+    public async Task<List<TopInstrumentModel>> GetTopViewedInstruments([FromQuery] int limit,
+        CancellationToken cancellationToken)
+    {
+        var response = await analyticsService.GetTopViewedInstrumentIdsAsync(limit, cancellationToken);
+
         return response;
     }
 }
