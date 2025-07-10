8414a2c33cbe677c0f84b470dd72ddb40591cd96
AntonShcherba <scherba891@gmail.com>
2025-07-10 12:07:31 +0300

    add AuthorizationExtensions

---
+++ AnalyticsService/AnalyticsService.API/Extensions/AuthorizationExtensions.cs
@@ -0,0 +1,36 @@
+using AnalyticsService.API.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AnalyticsService.API.Extensions;
+
+public static class AuthorizationExtensions
+{
+    public static IServiceCollection AddJwtAuthorization(this IServiceCollection services, IConfiguration configuration)
+    {
+        var authOptions = configuration.GetSection(nameof(AuthOptions)).Get<AuthOptions>()!;
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.Authority = authOptions.Authority;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidAudiences = authOptions.ValidAudiences,
+                    ValidateAudience = true,
+                    ValidateLifetime = true
+                };
+            });
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy("ReadAccess", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.RequireClaim("scope", "analyticsapi.read");
+            });
+
+        return services;
+    }
+}
