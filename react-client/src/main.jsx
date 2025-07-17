b0c5f99c82db9442c826618db909beba462e0782
AntonShcherba <scherba891@gmail.com>
2025-07-17 11:31:12 +0300

    add husky and eslint

--- react-client/src/main.jsx
+++ react-client/src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppProviders from './app/providers' 
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
+import React from 'react';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <AppProviders>
       <RouterProvider router={router} />
     </AppProviders>
-  </StrictMode>,
+  </React.StrictMode>,
 )
