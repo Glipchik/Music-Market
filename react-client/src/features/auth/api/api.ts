bf82d9d0918cf43299348352b64cd0bf4eb354dd
AntonShcherba <scherba891@gmail.com>
2025-07-24 17:27:35 +0300

    rename api.ts to index.ts and add error handling logic to it

--- react-client/src/features/auth/api/api.ts
+++
@@ -1,8 +0,0 @@
-import api from "@/shared/api/axiosInstance";
-import type { Claim } from "../model/types";
-
-export const getUserInfo = async () => {
-  const response = await api.get<Claim[]>("/bff/user");
-
-  return response;
-};
